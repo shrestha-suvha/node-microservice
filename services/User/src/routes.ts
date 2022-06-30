3a4894b24b5b43611ceff601d6f3c808755d3797
suvhashrestha <shrestha.suvha@gmail.com>
2022-06-30 10:04:30 +0545

    Handle path not found

--- services/User/src/routes.ts
+++ services/User/src/routes.ts
@@ -7,6 +7,9 @@ const router = Router();
 const routes = () => {
     router.post('/createUser', UserController.createUser);
     router.get('/getUser', UserController.getUser);
+    router.get('*', function(req, res){
+        res.status(404).send('Path not found');
+      });
     return router;
 };
 
